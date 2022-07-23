4f4dc82501bfa593a9a4a13b21598d3e67f941d3
German Shein <germanshein1995@gmail.com>
2022-07-23 23:37:13 +0200

    Uploading the Seven-Sided Die Simulation via a Five-Sided Die

--- Simulate_Seven_Sided_Die.js
+++ Simulate_Seven_Sided_Die.js
@@ -3,9 +3,59 @@ const Random_Number_Between_1_and_5 = () =>
 	return (Math.floor (Math.random () * 5) + 1);
 }
 
+/*
+
 const Random_Number_Between_1_and_7 = () =>
 {
 	return (Random_Number_Between_1_and_5 () % 2 + Random_Number_Between_1_and_5 () % 2 + Random_Number_Between_1_and_5 () % 2 + Random_Number_Between_1_and_5 () % 2 + Random_Number_Between_1_and_5 () % 2 + Random_Number_Between_1_and_5 () % 2 + Random_Number_Between_1_and_5 () % 2);
+}
+
+*/
+/*
+
+const Random_Number_Between_1_and_7 = () =>
+{
+	return (Random_Number_Between_1_and_5 () + Random_Number_Between_1_and_5 () % 3);
+}
+
+*/
+
+const Random_Number_Between_1_and_7 = () =>
+{
+	let Roll_1 = Random_Number_Between_1_and_5 ();
+	let Roll_2 = Random_Number_Between_1_and_5 () % 3;
+	let Sum = Roll_1 + Roll_2;
+	if (Sum === 1 || Sum === 7)
+	{
+		return Sum;
+	}
+	if (Sum === 2 || Sum === 6)
+	{
+		Roll_1 = Random_Number_Between_1_and_5 ();
+		Roll_2 = Random_Number_Between_1_and_5 () % 3;
+		Sum = Roll_1 + Roll_2;
+		if (Sum === 2 || Sum === 6)
+		{
+			return Sum;
+		}
+	}
+	if (Sum === 3 || Sum === 4 || Sum === 5)
+	{
+		Roll_1 = Random_Number_Between_1_and_5 ();
+		Roll_2 = Random_Number_Between_1_and_5 () % 3;
+		Sum = Roll_1 + Roll_2;
+		if (Sum === 3 || Sum === 4 || Sum === 5)
+		{
+			Roll_1 = Random_Number_Between_1_and_5 ();
+			Roll_2 = Random_Number_Between_1_and_5 () % 3;
+			Sum = Roll_1 + Roll_2;
+			if (Sum === 3 || Sum === 4 || Sum === 5)
+			{
+				return Sum;
+			}
+		}
+	}
+	return Sum;
 }
 
 let Output = "";
